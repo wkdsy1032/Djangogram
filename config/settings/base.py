25cdfe6bb4e4029876945d2cf6b415704b8ca9db
wkdsy1032 <wkdsy1032@naver.com>
2023-07-24 23:43:44 +0900

    20230724

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,8 +84,9 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "djangogram.users",
+    "djangogram.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "djangogram.posts.apps.PostsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
