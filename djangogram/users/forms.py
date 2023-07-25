9caa4bce2171953f40954e3f7966c67680cd0965
wkdsy1032 <wkdsy1032@naver.com>
2023-07-26 03:20:26 +0900

    20230726

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,41 +1,55 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
-from django.contrib.auth import forms as admin_forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, forms
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
 
 User = get_user_model()
 
 
-class UserAdminChangeForm(admin_forms.UserChangeForm):
-    class Meta(admin_forms.UserChangeForm.Meta):
+class UserAdminChangeForm(forms.UserChangeForm):
+    class Meta(forms.UserChangeForm.Meta):
         model = User
 
 
-class UserAdminCreationForm(admin_forms.UserCreationForm):
-    """
-    Form for User Creation in the Admin Area.
-    To change user signup, see UserSignupForm and UserSocialSignupForm.
-    """
+class UserAdminCreationForm(forms.UserCreationForm):
 
-    class Meta(admin_forms.UserCreationForm.Meta):
+    error_message = forms.UserCreationForm.error_messages.update(
+        {"duplicate_username": _("This username has already been taken.")}
+    )
+
+    class Meta(forms.UserCreationForm.Meta):
         model = User
-        error_messages = {
-            "username": {"unique": _("This username has already been taken.")},
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise ValidationError(self.error_messages["duplicate_username"])
+
+class SignUpForm(django_forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+
+        labels = {
+            'email': '이메일 주소',
+            'name': '성명',
+            'username': '사용자 이름',
+            'password': '비밀번호',
         }
 
-
-class UserSignupForm(SignupForm):
-    """
-    Form that will be rendered on a user sign up section/screen.
-    Default fields will be added automatically.
-    Check UserSocialSignupForm for accounts created from social.
-    """
-
-
-class UserSocialSignupForm(SocialSignupForm):
-    """
-    Renders the form when user has signed up using social accounts.
-    Default fields will be added automatically.
-    See UserSignupForm otherwise.
-    """
+        widgets = {
+            'password': django_forms.PasswordInput(),
+        }
+    
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user    
